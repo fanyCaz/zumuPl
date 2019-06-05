4870d110eca91091665ad6f39c8b03026309d862
Estefania Caceres <estefania@teknik.mx>
2019-06-05 17:20:39 -0500

    Impresion correcta de tablero

--- index.pl
+++ index.pl
@@ -51,14 +51,6 @@ sub cuadro {
 			elsif($simbolicPlaces[$i][$j]==1000){
 				print "|  <|";
 			}
-			# elsif($simbolicPlaces[$i][$j]!=1000 and $simbolicPlaces[$i][$j]!=2000 and defined $simbolicPlaces[$i][$j]){
-			# 	print $simbolicPlaces[$i][$j];
-			# }
-
-			# elsif($espacio==1 && $renglon != $j){
-			# 	print "|  |";
-			# 	$espacio=0;
-			# }
 			else{
 				print "|  |";
 				$espacio=0;
@@ -75,8 +67,8 @@ sub cuadroConNumeros {
 			if($simbolicPlaces[$i][$j]==2000){
 				if(defined $addedNumbers[$i][$j]){
 					my $print = $addedNumbers[$i][$j];
-					$print =~ s/^$//ms;      # Remove completely empty line
-					print "|$print]>|";
+					chop $print;     # Match! $str is now in UTF-8 format.
+					print '| '.$print.'>';
 				}
 				else{
 					print "|  >";
@@ -90,21 +82,11 @@ sub cuadroConNumeros {
 					print "|  <|";
 				}				
 			}
-			
-
-			# elsif($simbolicPlaces[$i][$j]!=1000 and $simbolicPlaces[$i][$j]!=2000 and defined $simbolicPlaces[$i][$j]){
-			# 	print $simbolicPlaces[$i][$j];
-			# }
-
-			# elsif($espacio==1 && $renglon != $j){
-			# 	print "|  |";
-			# 	$espacio=0;
-			# }
 			else{
 				if(defined $addedNumbers[$i][$j]){
 					my $print = $addedNumbers[$i][$j];
-					$print =~ s/^$//ms;      # Remove completely empty line
-					print "|$print]|";
+					chop $print;     # Match! $str is now in UTF-8 format.
+					print '| '.$print.'|';
 				}
 				else{
 					print "|  |";
@@ -145,8 +127,9 @@ sub input {
 	}
 	while ($v < 1 || $v > $level);
 
-	print "(  $h , $v )\n";
-	
+	my $print= "(  $h , $v )\n";
+	chop $print;
+	print $print;
 	do{
 		print "Ingrese el numero que quiere ingresar : ";
 		$numero = <STDIN>;
@@ -183,9 +166,15 @@ sub Verificar {
 						}
 					}
 				}
+
 				if($correcto == $definidos){
-					print "Tienes un buen juego!\n";
-					return 1;
+					if($definidos == ($level*$level)){
+						return 3;
+					}
+					else{
+						print "Tienes un buen juego!\n";
+						return 1;
+					}
 				}
 				else{
 					print "Tienes al menos, un error\n";
@@ -258,15 +247,13 @@ sub Plantilla1 {
 	$simbolicPlaces[1][3]=2000;
 	$simbolicPlaces[2][1]=2;
 	$level = 4;
-	if(@_[0] == 1){
-		&cuadroConNumeros(@simbolicPlaces,$level,@addedNumbers);
-	}
-	else{
+	if(@_[0]==0){
 		&cuadro(@simbolicPlaces,$level);
 	}
 	@coordenadas=input($level);
 	$addedNumbers[$coordenadas[0]][$coordenadas[1]] = $coordenadas[2];
-	print "¿Ingresar otro número? Elija un digito : 1. Si 2. No";
+	&cuadroConNumeros(@simbolicPlaces,$level,@addedNumbers);
+	print "¿Ingresar otro número? Elija un digito : 1. Si 2. No : ";
 	my $verificacion = <STDIN>;
 	if($verificacion == 1){
 		&Plantilla1(1);
@@ -276,7 +263,7 @@ sub Plantilla1 {
 		if ($continuar == 1){
 			# my $continuar = &VerificarSimbolic(@addedNumbers);
 			# my $continuar = &VerificarSimbolic(@simbolicPlaces,@addedNumbers);
-			print "¿Deseas seguir jugando? Elija un digito : 1. Si 2. No";
+			print "¿Deseas seguir jugando? Elija un digito : 1. Si 2. No : ";
 			$continuar=<STDIN>;
 			if($continuar == 1){
 				&Plantilla1(1);
@@ -286,7 +273,7 @@ sub Plantilla1 {
 			}
 
 		}
-		else{
+		elsif($continuar == 2){
 			print "¿Deseas seguir jugando? Elija un digito : 1. Si 2. No";
 			$continuar=<STDIN>;
 			if($continuar == 1){
@@ -295,6 +282,9 @@ sub Plantilla1 {
 			else{
 				my $out = <STDIN>;
 			}
+		}
+		else{
+			
 		}
 		# my $continuar= &Verificar(@addedNumbers);
 		# print "Continuar :" .$continuar;
@@ -608,11 +598,13 @@ sub Plantilla10{
 
 #SCRIPT
 sub Menu {
-
-	print "Instrucciones cortas: ";
-	print "En 'Mainarizumu' se presenta un tablero vacio\n, en donde tendras que rellenar los recuadros con numeros del 1 hasta el numero de filas que juegues.\n";
-	print "Habra simbolos como: '>' mayor que, y '<' menor que.\nSi estos s1mbolos conectan dos recuadros, tendras que escribir valores que cumplan esta condicion.\n";
-	print "Si dos recuadros estan conectados por un numero,\n tendras que escribir valores en estos recuadros que cumplan una resta que de como resultado el numero que se presente.\n";
+	if(@_[0]==0){
+		print "Instrucciones cortas: ";
+		print "En 'Mainarizumu' se presenta un tablero vacio\n, en donde tendras que rellenar los recuadros con numeros del 1 hasta el numero de filas que juegues.\n";
+		print "Habra simbolos como: '>' mayor que, y '<' menor que.\nSi estos s1mbolos conectan dos recuadros, tendras que escribir valores que cumplan esta condicion.\n";
+		print "Si dos recuadros estan conectados por un numero,\n tendras que escribir valores en estos recuadros que cumplan una resta que de como resultado el numero que se presente.\n";	
+	}
+	
 	do{
 		print "Niveles a Jugar: \n"."Nivel 1: 4x4 \n";
 		# \n"."Nivel 2: 5x5 \n"."Nivel 3: 6x6 \n
